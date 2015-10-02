514435c00e806cd7fbc8f41872ceb56898253fe1
feverdoingwork <feverdoingwork@gmail.com>
2015-10-02 23:20:51 +0000

    up to 9

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
   
+  def destroy
+    @article = Article.find_by_id params[:article_id]
+    @comment = Comment.find_by_id params[:id]
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+  
   private
   
   def comments_params
