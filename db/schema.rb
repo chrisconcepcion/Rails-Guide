41fd887bbba0b0ede615bfb2a0b24f15dee419ce
feverdoingwork <feverdoingwork@gmail.com>
2015-10-17 21:34:51 +0000

    added user model, table and created association between user and article models

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002214403) do
+ActiveRecord::Schema.define(version: 20151017213126) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -26,6 +27,13 @@ ActiveRecord::Schema.define(version: 20151002214403) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email"
+    t.string   "username"
   end
 
 end
